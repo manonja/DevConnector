35381eb2c03ea206cf9f4ce4419dc2ecf8bc1d84
Manon Jacquin <manon.jacquin@gmail.com>
2019-07-23 15:19:31 +0100

    Clean up and initial components

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react'
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+
+
+const App = () => {
+  return (
+    <Fragment>
+    <Navbar/>
+    <Landing/>   
+</Fragment>
+  )
+}
+
+export default App
