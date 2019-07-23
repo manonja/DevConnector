a3ab033c10c916d8c73e5b05d30cafa8916054cc
Manon Jacquin <manon.jacquin@gmail.com>
2019-07-23 17:31:02 +0100

    Build register form and useState hooks implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React, {Fragment} from 'react'
-import Navbar from './components/layouts/Navbar';
-import Landing from './components/layouts/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Navbar from './components/layouts/Navbar'
+import Landing from './components/layouts/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
+
 
 
 const App = () => {
   return (
-    <Fragment>
-    <Navbar/>
-    <Landing/>   
-</Fragment>
+    <Router>
+      <Fragment>
+          <Navbar/>
+          <Route exact path='/' component={Landing} />   
+          <section className='container'>
+            <Switch>
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/register' component={Register} />
+
+            </Switch>
+          </section>
+      </Fragment>
+    </Router>
+   
   )
 }
 
