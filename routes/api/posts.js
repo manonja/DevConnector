46221638b7e6beb41d16d5c43505de3365ecc95d
Manon Jacquin <manon.jacquin@gmail.com>
2019-08-08 10:34:29 +0100

    Creates get request for post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,46 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = equire('express-validator/check')
+const auth = require('../../middleware/auth')
+
+// import models 
+const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
+const User = require('../../models/User')
+
+
 
 // @route     GET api/posts
 // @desc      Test route
-// @access    Public
-router.get('/', (req, res) => res.send('Post route'))
+// @access    Private
+router.get('/', [auth, [
+  check('text', 'Text is required')
+      .not()
+      .isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array() })
+  }
+
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = new Post({
+      text = req.body.text,
+      name = user.name,
+      avatar = user.avatar,
+      user = req.user.id
+    })
+
+    const post = await newPost.save()
+    res.json(post)
+
+  } catch(err){
+    console.error(err)
+    res.status(500).send({msg: 'Server error'})
+  }
+  
+})
 
 module.exports = router
