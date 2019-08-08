066dd4898bb0efdcbe1d12150cb6b49819d947d7
Manon Jacquin <manon.jacquin@gmail.com>
2019-08-08 09:42:48 +0100

    Creates put and delete request for education in user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -230,8 +230,85 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id})
     // Get remove index
-    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id)
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id)
+      
     profile.experience.splice(removeIndex, 1)
+    await profile.save
+    res.json(profile)
+
+  } catch(err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
+// @route     PUT api/profile/education
+// @desc      Add profile education
+// @access    Private
+router.put('/education', [auth, 
+  [
+    check('school', 'School is required')
+      .not()
+      .isEmpty(),
+    check('degree', 'Degree are required')
+      .not()
+      .isEmpty(),
+    check('field', 'Field of study date are required')
+    .not()
+    .isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { 
+    school, 
+    degree,
+    field,
+    location,
+    from, 
+    to, 
+    description
+  } = req.body
+
+  const newEd = {
+    school, 
+    degree,
+    field,
+    location,
+    from, 
+    to, 
+    description
+  }
+
+  try {
+    const profile = await Profile.findOne({ user: req.user.id})
+    profile.education.unshift(newEd)
+    await profile.save
+    res.json(profile)
+
+  } catch(err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+
+})
+
+// @route     DELETE api/profile/education/:exp_id
+// @desc      Delete education from user profile
+// @access    Private
+router.delete('/experience/:ed_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id})
+    // Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.ed_id)
+
+    profile.education.splice(removeIndex, 1)
     await profile.save
     res.json(profile)
     
