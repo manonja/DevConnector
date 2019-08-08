8376dcb4fe2c77d8ee6b439e1ba4ec03fdd5bc4a
Manon Jacquin <manon.jacquin@gmail.com>
2019-08-08 10:12:46 +0100

    Creates get request for github repos on profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const request = require('request')
+const config = require('config')
+
 const router = express.Router()
 const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator/check')
@@ -233,7 +236,7 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     const removeIndex = profile.experience
       .map(item => item.id)
       .indexOf(req.params.exp_id)
-      
+
     profile.experience.splice(removeIndex, 1)
     await profile.save
     res.json(profile)
@@ -300,6 +303,7 @@ router.put('/education', [auth,
 // @route     DELETE api/profile/education/:exp_id
 // @desc      Delete education from user profile
 // @access    Private
+
 router.delete('/experience/:ed_id', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id})
@@ -317,6 +321,36 @@ router.delete('/experience/:ed_id', auth, async (req, res) => {
     res.status(500).send('Server error')
   }
 })
+
+// @route     GET api/profile/github/:username
+// @desc      Get github profile 
+// @access    Public
+
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')
+      }&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: {'user-agent': 'node.js'}
+    };
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error)
+      if(response.statusCode !== 200){
+        return res.status(404).json({ msg: 'No GitHub profile found' })
+      }
+
+      res.json(JSON.parse(body))
+    })
+  } catch(err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
 
 
 
